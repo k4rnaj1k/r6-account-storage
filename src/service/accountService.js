395faa1909a1c9a692f90b2bd4038ccccfcddab4
k4rnaj1k <karnajikplayer@gmail.com>
2023-04-18 02:12:11 +0300

    Improve building, update local file path

--- src/service/accountService.js
+++ src/service/accountService.js
@@ -1,5 +1,7 @@
+const { app } = require('electron');
 const { AES, enc } = require('crypto-js');
 const fs = require('fs');
+const path = require('path');
 
 const decodePassword = (password, passKey) => {
     return AES.decrypt(password, passKey).toString(enc.Utf8);
@@ -9,21 +11,25 @@ const encodePassword = (password, passKey) => {
     return AES.encrypt(password, passKey).toString();
 }
 
+const getAccountsFilePath = () => {
+    const userDataPath = app.getPath('userData');
+    return path.join(userDataPath, 'accounts.json');
+};
+
 const addAcount = (username, password, passKey) => {
-    const fileData = fs.readFileSync('accounts.json', { encoding: 'utf-8', flag: 'a+' });
+    const fileData = fs.readFileSync(getAccountsFilePath(), { encoding: 'utf-8', flag: 'a+' });
     let res = [];
     if(fileData){
         res = JSON.parse(fileData);
     }
     res.push({ username, password: encodePassword(password, passKey) });
-    fs.writeFileSync('accounts.json', JSON.stringify(res, null, 4), { encoding: 'utf-8', flag: 'w+' });
+    fs.writeFileSync(getAccountsFilePath(), JSON.stringify(res, null, 4), { encoding: 'utf-8', flag: 'w+' });
     return true;
 }
 
 const readAccounts = (passKey) => {
     //account file structure: account, password(encoded)
-    const fileData = fs.readFileSync('accounts.json', { encoding: 'utf-8', flag: 'a+' });
-    let res = [];
+    const fileData = fs.readFileSync(getAccountsFilePath(), { encoding: 'utf-8', flag: 'a+' });    let res = [];
     if (fileData) {
         res = JSON.parse(fileData);
     }
